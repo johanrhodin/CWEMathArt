515cd72c4ac378dc90c4d86d068b969bf9882cbf
johanrhodin <johan.rhodin@gmail.com>
2017-02-22 22:32:25 -0600

    Add experiments folder

---
+++ experiments/delaunay.m
@@ -0,0 +1,24 @@
+(* ::Package:: *)
+
+(* ::Text:: *)
+(*Feb 22, 2017*)
+
+
+(* ::Input:: *)
+(*pts2=Flatten[Table[{i,j},{i,1,10},{j,1,i}],1];*)
+
+
+(* ::Input:: *)
+(*pts3=Flatten[Table[{j,i},{i,1,10},{j,1,i}],1];*)
+
+
+(* ::Input:: *)
+(*dm1=DelaunayMesh[pts2,MeshCellStyle->{{1,All}->Black},BaseStyle->Orange,Background->Gray];*)
+
+
+(* ::Input:: *)
+(*dm2=DelaunayMesh[pts3,MeshCellStyle->{{1,All}->Black},BaseStyle->Green,Background->Gray];*)
+
+
+(* ::Input:: *)
+(*Show[dm1,dm2]*)
